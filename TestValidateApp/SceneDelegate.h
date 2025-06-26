26ec82b413c24cd03b487347f6e1194e428e27cf
Tung Ngo <tungngo@coccoc.com>
2025-06-26 14:43:26 +0700

    First commit

---
+++ TestValidateApp/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TestValidateApp
+//
+//  Created by tungngo on 26/6/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TestValidateApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestValidateApp
+//
+//  Created by tungngo on 26/6/25.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
